1f433a8aed3365109486d5d511585eefddda11b1
David Belac <delobelac@gmail.com>
2026-03-19 18:43:46 +0100

    added class parser

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include "Token.h"
 #include "Scanner.h"
+#include "Parser.h"
 #include <stdexcept>
 #include <exception>
 
@@ -27,93 +28,6 @@ string getTerminalName(int tokenID) {
     }
 }
 
-void Bitwise(Scanner& scanner, Token& currentToken);
-void BitwiseApostroph(Scanner& scanner, Token& currentToken);
-void Additive(Scanner& scanner, Token& currentToken);
-void AdditiveApostroph(Scanner& scanner, Token& currentToken);
-void Multiplicative(Scanner& scanner, Token& currentToken);
-void MultiplicativeApostroph(Scanner& scanner, Token& currentToken);
-void Unary(Scanner& scanner, Token& currentToken);
-void Primary(Scanner& scanner, Token& currentToken);
-
-void Primary(Scanner& scanner, Token& currentToken) {
-    int type = currentToken.getToken();
-    if (type == Scanner::tInt || type == Scanner::tHex || type == Scanner::tVariable) {
-        currentToken = scanner.nextToken();
-    } else if (type == Scanner::tLparen) {
-        currentToken = scanner.nextToken();
-        Bitwise(scanner, currentToken);
-        if (currentToken.getToken() == Scanner::tRparen) {
-            currentToken = scanner.nextToken();
-        } else {
-            throw std::runtime_error(
-                "Pričakovan ')', najden: " + currentToken.getLexem() +
-                " na (" + std::to_string(currentToken.getRow()) + "," +
-                std::to_string(currentToken.getColumn()) + ")"
-            );
-        }
-    } else {
-        throw std::runtime_error(
-            "Nepričakovan simbol: " + currentToken.getLexem() +
-            " na (" + std::to_string(currentToken.getRow()) + "," +
-            std::to_string(currentToken.getColumn()) + ")"
-        );
-    }
-}
-
-void Unary(Scanner& scanner, Token& currentToken) {
-    int type = currentToken.getToken();
-    if (type == Scanner::tPlus || type == Scanner::tMinus) {
-        currentToken = scanner.nextToken();
-        Primary(scanner, currentToken);
-    } else {
-        Primary(scanner, currentToken);
-    }
-}
-
-void MultiplicativeApostroph(Scanner& scanner, Token& currentToken) {
-    if (currentToken.getToken() == Scanner::tTimes || currentToken.getToken() == Scanner::tDivide ||
-        currentToken.getToken() == Scanner::tModulo) {
-        currentToken = scanner.nextToken();
-        Unary(scanner, currentToken);
-        MultiplicativeApostroph(scanner, currentToken);
-    }
-}
-
-void AdditiveApostroph(Scanner& scanner, Token& currentToken) {
-    if (currentToken.getToken() == Scanner::tPlus || currentToken.getToken() == Scanner::tMinus) {
-        currentToken = scanner.nextToken();
-        Multiplicative(scanner, currentToken);
-        AdditiveApostroph(scanner, currentToken);
-    }
-}
-
-void BitwiseApostroph(Scanner& scanner, Token& currentToken) {
-    if (currentToken.getToken() == Scanner::tBwand || currentToken.getToken() == Scanner::tBwor) {
-        currentToken = scanner.nextToken();
-        Additive(scanner, currentToken);
-        BitwiseApostroph(scanner, currentToken);
-    }
-}
-
-void Multiplicative(Scanner& scanner, Token& currentToken) {
-    Unary(scanner, currentToken);
-    MultiplicativeApostroph(scanner, currentToken);
-}
-
-void Additive(Scanner& scanner, Token& currentToken) {
-    Multiplicative(scanner, currentToken);
-    AdditiveApostroph(scanner, currentToken);
-}
-
-void Bitwise(Scanner& scanner, Token& currentToken) {
-    Additive(scanner, currentToken);
-    BitwiseApostroph(scanner, currentToken);
-}
-
-void Expr(Scanner& scanner, Token& currentToken){
-    Bitwise(scanner, currentToken);
-}
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
@@ -133,7 +47,7 @@ int main(int argc, char* argv[]) {
 
     currentToken = scanner.nextToken();
     try {
-        Expr(scanner, currentToken);
+        Parser::Expr(scanner, currentToken);
         if (currentToken.isEof() && currentToken.getLexem() == "") cout << "accept" << endl;
         else cout << "reject" << endl;
     }catch (std::exception& e) {
